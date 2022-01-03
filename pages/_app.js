4ab65c4a896ddd1dc34332e45668863e84de87dc
IvanLuksic <ivanluksic1@gmail.com>
2022-01-03 19:00:16 +0100

    Created MUI theme, added favicon, started boilerplating

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Kreiranje teme i wrappanje komponenti da su primary(TR plava) i secondary(TR žuta)
+// boje dostupni i primjenjeni u mui kroz cijeli projekt.
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5C76FE",
+    },
+    secondary: {
+      main: "#FFCC03",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+            <ThemeProvider theme={theme}>
+               <Component {...pageProps} />
+            </ThemeProvider>
+         )
 }
 
 export default MyApp
