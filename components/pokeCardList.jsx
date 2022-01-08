9d3841db98a00c32b33dbcd7beb0fb5c46c638d5
IvanLuksic <ivanluksic1@gmail.com>
2022-01-08 17:17:43 +0100

    Added buy card and buy page, cart not yet implemented

--- components/pokeCardList.jsx
+++ components/pokeCardList.jsx
@@ -10,13 +10,16 @@ export default function PokeCardList({searchResult, isPokedex}) {
     return (
         <Grid container direction="row" justifyContent="center" alignItems="center" sx={{width: '100%', mt: "2em",mb: "2em"}}>
             {searchResult.map( value => (PokemonList[value - 1])).map( ({id,name, type}) => (
-                <Link href={`/pokemon/${name}`} key={Math.random()} passHref>
-                    <Grid item md={5} xs={11} sx={{margin: "0.5em", cursor:"pointer"}}>
-                       { isPokedex ? (<PokedexCard name={name} id={id} type={type[0]} />) :
-                                     (<BuyCard name={name} id={id} type={type[0]} price={Math.round(Math.random()*150+1)}/>)}
-                    </Grid>
-                </Link>)
-            )}
+                 isPokedex ? 
+                    (<Link href={`/pokemon/${name}`} key={Math.random()} passHref>
+                        <Grid item md={5} xs={11} sx={{margin: "0.5em", cursor:"pointer"}}>
+                            <PokedexCard name={name} id={id} type={type[0]} />                
+                        </Grid>
+                    </Link>) :
+                (<Grid item md={5} xs={11} sx={{margin: "0.5em"}} key={Math.random()}>
+                    <BuyCard name={name} id={id} type={type[0]} price={Math.round(Math.random()*150+1)+'.00€'} isLogged={true}/>
+                 </Grid>)
+            ))}
         </Grid>
     )
 }
---
+++ pages/buy/index.js
@@ -0,0 +1,32 @@
+import { Grid, Typography } from '@mui/material'
+import React, {useState} from 'react'
+import ContentContainer from '../../components/contentContainer'
+import Search from '../../components/search'
+import PokeCardList from '../../components/pokeCardList'
+import PokemonList from '../../staticFiles/pokemonList.json'
+import TypeToColorList from '../../staticFiles/correspondingTypeColor.json'
+import PageHeading from '../../components/pageHeading'
+
+export default function Buy() {
+
+    let [searchResult,setSearchResult] = useState(Array.from({length: 11}, () => Math.floor((Math.random() * 150) + 1)));
+    /*console.log(new Array(11).fill().map((a, i) => a = i).sort(() => Math.random() - 0.5));
+
+    console.log(JSON.stringify(PokemonList.map(({id, name, type}) => ( {id, name, type}))))
+    console.log(AutocompleteList)*/
+    return (
+        <div className='container'>
+            <ContentContainer>
+                <Grid container direction="row" justifyContent="center" alignItems="center"  sx={{width: "100%"}}>
+                    <PageHeading heading="Buy NFTs" subheading="Buy NFT Pokemon pictures"/>
+                    <Grid item md={12} xs={11} >
+                        <Search autocompleteList={PokemonList} searchType='pokemon' chipList={Object.getOwnPropertyNames(TypeToColorList)} setSearchResult={setSearchResult}/>
+                    </Grid>
+                    <Grid item md={12} xs={11}>
+                        <PokeCardList searchResult={searchResult} isPokedex={false}/>
+                    </Grid>
+                </Grid>
+            </ContentContainer>
+        </div>
+    )
+}
