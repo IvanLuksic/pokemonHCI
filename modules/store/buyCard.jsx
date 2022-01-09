2efb131f5156f8b9d44e2bf8d1c4494296c3a07b
IvanLuksic <ivanluksic1@gmail.com>
2022-01-09 21:00:12 +0100

    Added desktop cart

--- modules/store/buyCard.jsx
+++ modules/store/buyCard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import SpeechBubble from '../../staticFiles/speechBubble.svg'
 
 
-export default function BuyCard({name, id, type, price, isLogged}) {
+export default function BuyCard({name, id, type, price, isLogged, setCartItems}) {
     return (
         <Paper elevation={1} sx={{width: "100%", height: "100%", backgroundColor:"#F5F5F5",border: "2px solid", borderColor: "primary.main", borderRadius: 0}}>
             <Grid container direction="row" justifyContent="center" alignItems="center" >
@@ -22,7 +22,7 @@ export default function BuyCard({name, id, type, price, isLogged}) {
                         </Box>
                 </Grid>
                 <Grid item xs={12}>
-                    <Button variant="outlined" color='primary' disabled={!isLogged} size="large" sx={{width: '100%', borderRadius: 0, borderColor: 'primary.main',transform: 'scale(1.01)'}}>Buy</Button>
+                    <Button variant="outlined" onClick={()=> setCartItems(old=> [...old,{name: name, id: id, price: price}])} color='primary' disabled={!isLogged} size="large" sx={{width: '100%', borderRadius: 0, borderColor: 'primary.main',transform: 'scale(1.01)'}}>Buy</Button>
                 </Grid>
             </Grid>
         </Paper>
--- pages/buy/index.js
+++ pages/buy/index.js
@@ -1,13 +1,24 @@
 import { Grid } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ContentContainer from '../../components/contentContainer'
 import Search from '../../components/search'
 import PokeCardList from '../../components/pokeCardList'
 import PokemonList from '../../staticFiles/pokemonList.json'
 import TypeToColorList from '../../staticFiles/correspondingTypeColor.json'
 import PageHeading from '../../components/pageHeading'
+import Cart from '../../modules/store/cart'
 
 export default function Buy() {
+
+    let [cartItems, setCartItems] = useState([]);
+    let isCart = false
+
+    useEffect(() => {
+        if(cartItems.length > 0){
+            isCart = true
+        }
+
+    }, [cartItems])
 
     let [searchResult, setSearchResult] = useState(Array.from({ length: 11 }, () => Math.floor((Math.random() * 150) + 1)));
     /*console.log(new Array(11).fill().map((a, i) => a = i).sort(() => Math.random() - 0.5));
@@ -23,9 +34,11 @@ export default function Buy() {
                         <Search autocompleteList={PokemonList} searchType='pokemon' chipList={Object.getOwnPropertyNames(TypeToColorList)} setSearchResult={setSearchResult} />
                     </Grid>
                     <Grid item md={12} xs={11}>
-                        <PokeCardList searchResult={searchResult} isPokedex={false} />
+                        <PokeCardList searchResult={searchResult} isPokedex={false} setCartItems={setCartItems}/>
                     </Grid>
+                    {cartItems.length ? <Cart cartItems={cartItems} setCartItems={setCartItems}/>: null}
                 </Grid>
+                
             </ContentContainer>
         </div>
     )
