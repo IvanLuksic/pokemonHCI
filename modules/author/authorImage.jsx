7ffe97e37c4909d976b94dba25f6355fb9c2d1ee
DSrhoj <srhojduje@gmail.com>
2022-01-09 02:22:13 +0100

    Fix author image border aspect

--- modules/author/authorImage.jsx
+++ modules/author/authorImage.jsx
@@ -8,8 +8,8 @@ export default function AuthorImage({authorImgUrl, authorName}) {
     const theme = useTheme()
     
     return (
-        <Paper elevation={3} sx={{ width: "100%", height: "100%", aspectRatio: "1", borderRadius: "50%", border: 3, borderColor: theme.palette.primary.main, overflow: "hidden"}}>
-            <Grid container direction="row" item xs sx={{ height: "100%", width: "100%", justifyContent: "center", alignItems: "center", textAlign: "center", lineHeight: "500%"}}>
+        <Paper elevation={3} sx={{ width: "100%", aspectRatio: "1", borderRadius: "50%", border: 3, borderColor: theme.palette.primary.main, overflow: "hidden"}}>
+            <Grid container direction="row" item xs sx={{width: "100%", aspectRatio: "1", justifyContent: "center", alignItems: "center", textAlign: "center", lineHeight: "500%"}}>
                     <Image src={authorImgUrl} width={"200%"} height={"200%"} alt={authorName} />
             </Grid>
         </Paper>
