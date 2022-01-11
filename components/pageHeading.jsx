6ddbab7cfc7aa47c9e6ff6d84eb3a7eee7323414
DSrhoj <srhojduje@gmail.com>
2022-01-11 14:59:34 +0100

    Pass style to heading component

--- components/pageHeading.jsx
+++ components/pageHeading.jsx
@@ -1,9 +1,9 @@
 import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
-export default function PageHeading({heading, subheading, xs, sm, md}) {
+export default function PageHeading({heading, subheading, xs, sm, md, sx}) {
     return (
-        <Grid item xs={xs != null ? xs : 12} sm={sm != null ? sm : null} md={md != null ? md : null} sx={{textAlign: 'center', mt: '1em'}} id="back-to-top-anchor">
+        <Grid item xs={xs != null ? xs : 12} sm={sm != null ? sm : null} md={md != null ? md : null} sx={{textAlign: 'center', mt: '1em', ...sx,}} id="back-to-top-anchor">
          <Typography variant='h3' color="primary" sx={{fontWeight: 700}}>{heading}</Typography>
          <Typography variant='h6' sx={{color: "lightgray", fontWeight: 600}} >{subheading}</Typography>
         </Grid>
