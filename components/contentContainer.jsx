8a277d19b2b2528951107f68a591ca80b4f8ae0f
DSrhoj <srhojduje@gmail.com>
2022-01-17 18:06:54 +0100

    Remove home page content container padding

--- components/contentContainer.jsx
+++ components/contentContainer.jsx
@@ -5,7 +5,15 @@ export default function ContentContainer(props) {
 	return (
 		<Grid container direction="row" justifyContent="center" alignItems="strech">
 			<Grid item xs={12} md={9} lg={8} sx={{ minHeight: "72.9vh" }}>
-				<Paper sx={{ marginTop: { md: "4.5em", sm: "4em", xs: "3.5em" }, height: "100%",mb: "5em" }} elevation={4}>
+				<Paper
+					sx={{
+						mt: { md: "4.5em", sm: "4em", xs: "3.5em" },
+						height: "100%",
+						mb: "5em",
+						...props.sxPaper
+					}}
+					elevation={4}
+				>
 					{props.children}
 				</Paper>
 			</Grid>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Grid } from "@mui/material"
 import ContentContainer from "../components/contentContainer"
-import HeroImage from "../modules/blog/heroImage"
+import HeroImage from "../modules/common/heroImage"
 import BuyPreview from "../modules/home/buyPreview"
 import PokedexPreview from "../modules/home/pokedexPreview"
 import DataSourceAPI from "../lib/DataSourceApi"
@@ -8,8 +8,8 @@ import DataSourceAPI from "../lib/DataSourceApi"
 export default function Home({ image }) {
 	return (
 		<div className="container">
-			<HeroImage {...image} sxContainer={{ height: "500px" }} />
-			<ContentContainer>
+			<HeroImage {...image} sxContainer={{ height: "500px", mt: { md: "4.5em", sm: "4em", xs: "3.5em" } }} />
+			<ContentContainer sxPaper={{ mt: 0 }}>
 				<Grid container direction="row" justifyContent="center" alignItems="center">
 					<Grid item xs={11}>
 						<PokedexPreview />
