aa1bd0e7b574fc849cd765fdbba07387f029f093
DSrhoj <srhojduje@gmail.com>
2022-01-20 13:12:09 +0100

    Change home cards buttons height

--- modules/home/buyPreview.jsx
+++ modules/home/buyPreview.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import PokeCardList from '../../components/pokeCardList'
-import PokemonInfoContainer from '../pokedex/pokemonInfoContainer'
-import Link from 'next/link'
-import { Button } from '@mui/material'
+import React from "react"
+import PokeCardList from "../../components/pokeCardList"
+import PokemonInfoContainer from "../pokedex/pokemonInfoContainer"
+import Link from "next/link"
+import { Button } from "@mui/material"
 
 export default function BuyPreview() {
-    return (
-        <PokemonInfoContainer fullWidth heading='Buy'>
-            <PokeCardList searchResult={Array.from({ length: 4 }, () => Math.floor(Math.random() * 150 + 1))}/>
-            <div style={{height: "3em"}}> <Link href="/buy" passHref>
-                <Button variant="contained" sx={{float: "right", mr: "1em", mt:"0.5em"}}>See more to buy</Button>
-            </Link></div>
-        </PokemonInfoContainer>
-    )
+	return (
+		<PokemonInfoContainer fullWidth heading="Buy">
+			<PokeCardList searchResult={Array.from({ length: 4 }, () => Math.floor(Math.random() * 150 + 1))} />
+			<div style={{ height: "4em" }}>
+				{" "}
+				<Link href="/buy" passHref>
+					<Button
+						variant="contained"
+						sx={{ float: "right", mr: "2em", mt: "0.5em", minHeight: { xs: "3em", md: "3.5em" } }}
+					>
+						See more to buy
+					</Button>
+				</Link>
+			</div>
+		</PokemonInfoContainer>
+	)
 }
